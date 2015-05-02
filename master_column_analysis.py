b2a2c6fb8eae67ea3300e9cbb8a173d556f2dc97
Matthew Bourque <bourque@stsci.edu>
2015-05-02 15:46:00 -0400

    Moved data subsetting to separate module, and added function to determine which amp is associated with a given pixel.

--- master_column_analysis.py
+++ master_column_analysis.py
@@ -19,11 +19,53 @@ from astropy.io import fits
 from astropy.io import ascii
 import numpy as np
 
+from threshold_vals import get_thresh
+
+# -----------------------------------------------------------------------------
+
+def classify_pixel(pixel, treshold_dict):
+    """
+    0 = Good
+    1 = Warm & Stable
+    2 = Warm & Unstable
+    3 = Hot & Stable
+    4 = Hot & Unstable
+    """
+
+    if pixel > threshold_dict['dark_current'] + (3 * stdev):
+        results_array[row,:] = 1
+    elif pixel < threshold_dict['dark_current'] - (3 * stdev):
+        results_array[row,:] = 2
+
+    return pixel_class
+
+# -----------------------------------------------------------------------------
+
+def get_amp(row, col):
+    """
+    Return the number of the amp associated with the given row and
+    column number
+    """
+
+    if row >= 2070 and col <= 2103:
+        return 0
+    elif row >= 2070 and col > 2103:
+        return 1
+    elif row < 2070 and col <= 2103:
+        return 2
+    elif row < 2070 and col > 2103:
+        return 3
+
+    return amp
+
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+
 if __name__ == '__main__':
 
     # Open master column image and read in data
     print('\tReading in master column image')
-    master_column_image = '/Users/bourque/Desktop/data_mining/Project/master_column_500.fits'
+    master_column_image = '/Users/bourque/Desktop/data_mining/Project/master_column_test.fits'
     with fits.open(master_column_image) as hdulist:
         data = hdulist[0].data
 
@@ -31,21 +73,13 @@ if __name__ == '__main__':
     print('\tReading in metadata')
     metadata_file = '/Users/bourque/Desktop/data_mining/Project/metadata.dat'
     metadata = ascii.read(metadata_file, guess=False, delimiter=',')
-    dates = metadata['EXPSTART']
-    print(dates)
 
-    # Subset the data for testing purposes
-    data = data[0:2069,3000:3500]
+    # Set dark current and thresholds
+    first_image = metadata['path'][0]
+    treshhold_dict = get_thresh(first_image)
 
-    # Save the test data to new image for visualization purposes
-    print('\tWriting out test image')
-    test_file = '/Users/bourque/Desktop/data_mining/Project/master_column_test.fits'
-    if not os.path.exists(test_file):
-        hdu = fits.PrimaryHDU(data)
-        hdu.writeto(test_file)
-
-    # Initialize results array
-    results_array = np.zeros((2070, 1))
+    # Initialize results array that will hold classes
+    results_array = np.zeros((data.shape[0], 1))
 
     # Iterate over columns and compute statistics
     number_columns = data.shape[1]
@@ -53,16 +87,10 @@ if __name__ == '__main__':
 
         print('Processing column {} of {}'.format(col+1, number_columns))
         column = data[:,col]
-        median = np.median(column)
-        stdev = np.std(column)
 
         for row in xrange(len(column)):
             pixel = column[row]
+            amp_num = get_amp(row, 500)
 
-            # Classify pixel
-            # 1 = hot, 2 = cold, 3 = warm, 4 =
-            if pixel > median + (3 * stdev):
-                results_array[row,:] = 1
-            elif pixel < median - (3 * stdev):
-                results_array[row,:] = 2
-
+            pixel_class = classify_pixel(pixel, threshold_dict)
+            result_array[row,:] = pixel_class
